e5099c01a8effb93e3e7e9d3863994cac131c056
jvMori <iwona.moryc92@gmail.com>
2020-02-13 19:28:52 +0100

    Initial commit

---
+++ app/src/main/java/com/jvmori/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jvmori.cryptocurrencyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
