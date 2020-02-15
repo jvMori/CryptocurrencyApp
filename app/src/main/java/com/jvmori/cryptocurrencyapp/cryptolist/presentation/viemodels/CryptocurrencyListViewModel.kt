fda3f88c350e4ceb890617f6092727ddc0ba2785
jvMori <iwona.moryc92@gmail.com>
2020-02-15 16:21:44 +0100

    [implemented] created viewmodel in order to prepare data for showing in view

---
+++ app/src/main/java/com/jvmori/cryptocurrencyapp/cryptolist/presentation/viemodels/CryptocurrencyListViewModel.kt
@@ -0,0 +1,40 @@
+package com.jvmori.cryptocurrencyapp.cryptolist.presentation.viemodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.jvmori.cryptocurrencyapp.cryptolist.data.util.Resource
+import com.jvmori.cryptocurrencyapp.cryptolist.domain.entities.CryptocurrencyEntity
+import com.jvmori.cryptocurrencyapp.cryptolist.domain.repositories.CryptocurrencyRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class CryptocurrencyListViewModel(
+    private val repository: CryptocurrencyRepository
+) : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+    private val _cryptocurrencies = MutableLiveData<Resource<List<CryptocurrencyEntity>>>()
+    val cryptocurrencies: LiveData<Resource<List<CryptocurrencyEntity>>>
+        get() = _cryptocurrencies
+
+    fun fetchCryptocurrencies(sort: String) {
+        _cryptocurrencies.value = Resource.loading(null)
+        disposable.add(
+            repository.getCryptocurrencies(sort)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ success ->
+                    _cryptocurrencies.value = Resource.success(success)
+                }, { error ->
+                    _cryptocurrencies.value = Resource.error(error.localizedMessage ?: "", null)
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+}
